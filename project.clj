0f0a38cd9b42c14fa327a3135ecbcf4cb0821c93
Veha Suwatphisankij <sky.48478@gmail.com>
2019-06-02 07:35:16 +0700

    start dbgram by setting 2 env that can run together (nextjs & shadow-cljs)

--- project.clj
+++ project.clj
@@ -1,4 +1,11 @@
-(defproject xyz.vehas "placholder for intelij" 
+(defproject xyz.vehas "placholder for intelij"
   :description "placholder for intelij"
   :url "https://vehas/xyz"
-  :source-paths ["src" "src/main"])
+  :source-paths ["src/main" "repl"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [reagent "0.8.1"]
+                 [thheller/shadow-cljs "1.0.20170629"]
+                 [re-frame "0.10.6"]
+                 [day8.re-frame/tracing "0.5.1"]
+                 [day8.re-frame/re-frame-10x "0.3.2-react16"]
+                 [binaryage/devtools "0.9.8"]])
--- src/main/blog/app.cljs
+++ src/main/blog/app.cljs
@@ -1,18 +1,25 @@
 (ns blog.app
   (:require
    [app.counter :as counter]
+   [app.dbgram :as dbgram]
    [reagent.core :as r]
    [shadow.next-js :as sn]
    [component.link :as link]))
 
 
-(defn counter
+(defn counter-app
   {:export true
    :next/page "app/counter"}
   [props]
   (r/as-element
     [counter/main]))
 
+(defn dbgram-app
+  {:export true
+   :next/page "app/dbgram"}
+  [props]
+  (r/as-element
+    [dbgram/main]))
 
 (defn page-app
   {:export true
@@ -22,5 +29,7 @@
    [:div
     [:h1 "app"]
     ; [counter-component]
-    [link/link "แอพ counter" "/app/counter"]]))
+    [link/link "แอพ counter" "/app/counter"]
+    [link/link "แอพ dbgram ตัวช่วยอกแบบ database ทั้ง sql และ nosql"
+               "/app/dbgram"]]))
 
