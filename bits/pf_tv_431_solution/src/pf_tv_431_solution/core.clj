4778654c81b70253e4e8aa44d4f00e8fb73917bc
Veha <veha.suwat@gmail.com>
2021-06-26 07:58:04 +0700

    pf tv 431: use recur

--- bits/pf_tv_431_solution/src/pf_tv_431_solution/core.clj
+++ bits/pf_tv_431_solution/src/pf_tv_431_solution/core.clj
@@ -2,31 +2,30 @@
   (:gen-class)
   (:import (clojure.lang ExceptionInfo)))
 
-(defn sep-recur [bstr  {:keys [group depth current-str] :as opt}]
-  (println "at :"
-         (vec bstr)
-         opt)
-  (cond
-    (or (= bstr "") (= bstr nil)) group
+(defn sep-recur [bstr]
+  (loop
+    [bstr bstr
+     {:keys [group depth current-str] :as opt} {:group []}]
+    (cond
+      (or (= bstr "") (= bstr nil)) group
 
-    (= (first bstr) \()
-    (do
-      (sep-recur (next bstr) (-> opt
-                                 (update :depth #(if % (inc %) 1))
-                                 (update :current-str #(str % (first bstr))))))
-    (= (first bstr) \))
-    (sep-recur (next bstr) (-> opt
-                               (update :depth #(if % (dec %) (throw (Exception. "wrong bracket"))))
-                               (update :group #(if (zero? (dec depth))
-                                                 (conj % (str current-str (first bstr)))
-                                                 %))
-                               (update :current-str #(if (zero? (dec depth))
-                                                       ""
-                                                       (str % (first bstr))))))))
+      (= (first bstr) \()
+      (recur (next bstr) (-> opt
+                             (update :depth #(if % (inc %) 1))
+                             (update :current-str #(str % (first bstr)))))
+      (= (first bstr) \))
+      (recur (next bstr) (-> opt
+                             (update :depth #(if % (dec %) (throw (Exception. "wrong bracket"))))
+                             (update :group #(if (zero? (dec depth))
+                                               (conj % (str current-str (first bstr)))
+                                               %))
+                             (update :current-str #(if (zero? (dec depth))
+                                                     ""
+                                                     (str % (first bstr)))))))))
 
 
 (defn clusters [bstr]
-  (sep-recur bstr {:group []}))
+  (sep-recur bstr))
 
 (clusters "") ;=> []
 (clusters "()") ;=> ["()"]
